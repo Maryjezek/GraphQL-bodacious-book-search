1328bb45a2bb909f56258a6ccb986cc85dbe84a2
Mary Braunger <maryjezek@aol.com>
2022-04-22 13:35:49 -0400

    changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Book Search Engine Starter Code
+
+
+URL of Published Application:
+https://frozen-bastion-83369.herokuapp.com/
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -33,16 +33,16 @@ const LoginForm = () => {
       });
 
       console.log(data);
-      //Auth.login()
-      // const response = await loginUser(userFormData);
+      Auth.login()
+      const response = await loginUser(userFormData);
 
-      // if (!response.ok) {
-      //   throw new Error("something went wrong!");
-      // }
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
 
-      // const { token, user } = await response.json();
-      // console.log(user);
-      // Auth.login(token);
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
     } catch (err) {
       console.error(err);
       // setShowAlert(true);
